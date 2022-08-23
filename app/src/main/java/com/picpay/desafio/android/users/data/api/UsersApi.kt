80d2d2e4f3b78e24b4483eb8436a2f019053df30
Gabriel Lira <liraferreiragabriel@gmail.com>
2022-08-23 11:54:39 -0300

    improving code

--- app/src/main/java/com/picpay/desafio/android/users/data/api/UsersApi.kt
+++ app/src/main/java/com/picpay/desafio/android/users/data/api/UsersApi.kt
@@ -1,13 +1,9 @@
 package com.picpay.desafio.android.users.data.api
 
 import com.picpay.desafio.android.users.data.api.response.UserResponse
-import retrofit2.Call
 import retrofit2.http.GET
 
 interface UsersApi {
-
-    @GET("users")
-    fun getUsersLegacy(): Call<List<UserResponse>>
 
     @GET("users")
     suspend fun getUsers(): List<UserResponse>
